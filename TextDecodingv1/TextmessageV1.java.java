acaf35dc0741383c20e0eecfcf43ac9fa4ff7bac
maddisonmldavis <78170228+maddisonmldavis@users.noreply.github.com>
2022-12-03 12:47:45 -0700

    Add files via upload

---
+++ TextDecodingv1/TextmessageV1.java.java
@@ -0,0 +1,46 @@
+/**
+ * The purpose of this program is to demonstrate the use of several 
+ * methods of the String class.
+ * 
+ * ©FLVS 2007
+ * @author B. Jordan 
+ * @version 03/08/07
+ */
+public class TextmessageV1 
+{
+	public static void main(String[ ] args)
+	{
+	    //determine the length of the String object called odString
+	    String oldString = "I love you, forever";
+	    System.out.println("Old string: " + oldString);
+	    int stringLength = oldString.length();
+	    System.out.println("Number of characters: " + stringLength);
+	    System.out.println();
+	   	
+	    //replace characters within the String object
+	    String replaceCharacters = oldString.replace("forever", "4eva");
+	    replaceCharacters = replaceCharacters.replace("I love you", "I<3U");
+	    System.out.println("Replacement characters: " + replaceCharacters);
+	    System.out.println();
+	    int stringLength2 = replaceCharacters.length();
+	    System.out.println("Replacment Charcters: " + stringLength2);
+	    System.out.println();
+	   	
+	  
+	    
+	    //remove all the vowels from the string object.
+	    String newString1 = oldString.replaceAll("[aeiou]", "");
+	    System.out.println("New string: " + newString1);
+	    System.out.println();
+	    
+	    //move first word to the end of the string object
+	    int positionOfSpace = oldString.indexOf(' ');
+	    String substring1 = oldString.substring(0, positionOfSpace);
+	    String substring2 = oldString.substring(positionOfSpace, stringLength);	    
+	    String newString2 = substring2 + " " + substring1;
+	    System.out.println("New substring: " + newString2);
+	    System.out.println();
+	    
+	}//end of main method
+}//end of class
+
---
+++ TextcoderV2/TextdecodeV2.java
@@ -0,0 +1,60 @@
+﻿/**
+ * The purpose of this program is to demonstrate the use of several 
+ * methods of the String class. And then turn to a scanner class 
+ *
+ */
+import java.util.Scanner;
+public class TextdecodeV2 {  
+	public static void main(String[ ] args)
+	{
+	    
+	    //determine the length of the String object called odString
+	    
+	    Scanner in;
+        in = new Scanner(System.in); 
+        System.out.print("/n");
+        
+        //user friendly orginal input 
+	    System.out.println("Enter sentence to be decoded:    ");
+        String first = in.next();
+        String rest = in.nextLine();
+        String whole = first + rest; 
+	    System.out.println("You entered :   " + whole);
+	    int stringLength = first.length();
+	    int stringLengthRest = rest.length();
+	    int stringLengthWhole = whole.length();
+	    System.out.println("Number of characters: " + stringLengthWhole);
+	    System.out.println();
+	    
+	     //replace characters within the String object
+	    String replaceCharacters = whole.replace("e", "3");
+	    replaceCharacters = replaceCharacters.replace("s" + "S", "5");
+	    replaceCharacters = replaceCharacters.replace("i", "7");
+	    replaceCharacters = replaceCharacters.replace("e", "3");
+        System.out.println("Replacement characters: " + replaceCharacters);
+	    System.out.println();
+	    int stringLength2 = replaceCharacters.length();
+	    System.out.println("Replacment Charcters: " + stringLength2);
+	    System.out.println();
+	    
+	    	    //remove all the vowels from the string object.
+	    String newString1 = whole.replaceAll("[aeiou]", "");
+	    System.out.println("New string: " + newString1);
+	    int stringLengthNewString = newString1.length();
+	    System.out.println( "Length after being shortened  :  " + stringLengthNewString );
+	    System.out.println();
+	    
+	    	    //move first word to the end of the string object
+	    int positionOfSpace = whole.indexOf(' ');
+	    String substring1 = whole.substring(0, positionOfSpace);
+	    String substring2 = whole.substring(positionOfSpace, stringLengthWhole);	    
+	    String newString2 = substring2 + " " + substring1;
+	    System.out.println("New substring: " + newString2);
+	    System.out.println();
+	   	
+	    
+	    
+	    
+	}//end of main method
+}//end of class
+
